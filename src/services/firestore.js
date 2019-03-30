0d6ee7cb1ec9708813c4d4fa8fa8f3f925fc5adc
ryanboris <ryan.boris@gmail.com>
2019-03-30 06:35:03 -0400

    :rotating_light: removing linter warnings

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,10 +1,5 @@
 import * as admin from 'firebase-admin'
-import { Firestore } from '@google-cloud/firestore'
 import * as functions from 'firebase-functions'
-
-const firestore = new Firestore()
-const settings = { timestampsInSnapshots: true }
-firestore.settings(settings)
 
 admin.initializeApp(functions.config().firebase)
 const db = admin.firestore()
