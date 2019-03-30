3aa4f9820346c05af5367cea7172b346d66b0dc0
ryanboris <ryan.boris@gmail.com>
2019-03-29 23:46:33 -0400

    :rotating_light: removing linter warnings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react'
 
-import Layout from "../components/Layout";
+import Layout from '../components/Layout'
 
 export default () => (
   <Layout>
     <h1>Hello world!</h1>
   </Layout>
-);
+)
+
+/**
+ * fixes issue
+ */
