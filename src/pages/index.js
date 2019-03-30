9f5dc83c269cf38a824867d51cce39d5e2f99cf6
ryanboris <ryan.boris@gmail.com>
2019-03-29 23:44:24 -0400

    :construction: adds components for navbar and layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+import Layout from "../components/Layout";
+
+export default () => (
+  <Layout>
+    <h1>Hello world!</h1>
+  </Layout>
+);
